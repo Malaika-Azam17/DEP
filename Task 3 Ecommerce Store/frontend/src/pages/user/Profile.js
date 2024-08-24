9510db500f08dbb81d895f4c4d73fddda69c1a08
Malaika-Azam17 <malaikaazam700@gmail.com>
2024-08-24 16:49:24 +0500

    Add files via upload

---
+++ Task 3 Ecommerce Store/frontend/src/pages/user/Profile.js
@@ -0,0 +1,128 @@
+import Layout from '../../components/layout/layout.js'
+import UserMenu from '../../components/layout/UserMenu.js';
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../../context/auth.js";
+import axios from "axios";
+const Profile = () => {
+  //context
+  const [auth, setAuth] = useAuth();
+
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  //get user data
+  useEffect(() => {
+    const { email, name, phone, address } = auth?.user;
+    setName(name);
+    setPhone(phone);
+    setEmail(email);
+    setAddress(address);
+  }, [auth?.user]);
+
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put("/api/v1/auth/profile", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (data?.errro) {
+       alert(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        alert("Profile Updated Successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong");
+    }
+  };
+  return (
+    <Layout title={"Your Profile"}>
+      <div className="container-fluid" style={{paddingTop:'100px',paddingBottom:"2rem",backgroundColor:"rgb(208, 178, 235)",marginTop:"3rem",marginBottom:"3rem"}}>
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <div className="form-container ">
+              <form onSubmit={handleSubmit}>
+                <h4 className="title text-center" >USER PROFILE</h4>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Name"
+                    autoFocus
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Email "
+                    disabled
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter Your Password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Phone"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Address"
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Profile;
