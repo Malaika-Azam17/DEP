4294ee7afb8a908019c6961adb4bd1ec15b76f2a
Malaika-Azam17 <malaikaazam700@gmail.com>
2024-08-24 16:41:09 +0500

    Add files via upload

---
+++ Task 3 Ecommerce Store/frontend/src/components/Spinner.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const Spinner = ({ path = "" }) => {
+  const [count, setCount] = useState(3);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevValue) => {
+        if (prevValue === 1) {
+          navigate(`/${path}`, { state: { from: location.pathname } });
+          clearInterval(interval);
+          return 0;
+        }
+        return prevValue - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [navigate, location, path]);
+
+  return (
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <h1 className="text-center">Redirecting you in {count} seconds</h1>
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
